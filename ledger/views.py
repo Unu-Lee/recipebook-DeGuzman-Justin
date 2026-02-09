12027893bf9e6fdd1a5c73a471d6081b0dfcb68e
Justin <justinheindrich95@gmail.com>
2026-02-09 16:07:09 +0800

    Implemented template inheritance to recipe list html and added recipe list views

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+Recipe_list = [
+        {   
+            "name" : "Recipe 1", 
+            "ingredients": [
+                {"name": "tomato", "quantity": "3pcs"},
+                {"name": "onion", "quantity": "1pc"},
+                {"name": "pork", "quantity": "1kg"},
+                {"name": "water", "quantity": "1L"}
+                {"name": "sinigang mix", "quantity": "1 packet"}
+            ]
+        },
+        {
+            "name" : "Reciper 2",
+            "ingredients" : [
+                {"name" : "garlic", "quantity": "1 head"},
+                {"name" : "onion", "quantity": "1pc"},
+                {"name" : "vinegar", "quantity": "1/2cup"}
+                {"name" : "water", "quantity": "1 cup"}
+                {"name" : "salt", "quantity": "1 tablespoon"}
+                {"name" : "whole black peppers", "quantity": "1 tablespoon"}
+                {"name" : "pork", "quantity": "1 kilo"}
+            ]
+
+        }
+]
+
+def recipe_list(request):
+    ctx = {"recipes": Recipe_list}
+    return render (request, "ledger/recipe_list.html",ctx)
