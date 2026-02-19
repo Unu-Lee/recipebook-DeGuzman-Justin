a9859f64c764818173a76412ab18662ebe9d1e90
Justin <justinheindrich95@gmail.com>
2026-02-19 19:38:58 +0800

    Made final changes to naming of variables and correct syntax and re migrated the changes

--- ledger/models.py
+++ ledger/models.py
@@ -14,7 +14,7 @@ class Recipe(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('specific_recipe', args = [str(self.name)])
+        return reverse('specific_recipe', args = [str(self.pk)])
     
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
