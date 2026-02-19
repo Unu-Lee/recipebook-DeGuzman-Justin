069e3ff05dcae888072327d5b4af191a12daeeb3
Justin <justinheindrich95@gmail.com>
2026-02-19 23:32:32 +0800

    added absolute url for Ingredients to fit the specs

--- ledger/models.py
+++ ledger/models.py
@@ -9,6 +9,8 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse('home')
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
