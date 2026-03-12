fec0e4f19e9334faa4895235bf3660e951859d7f
Justin <justinheindrich95@gmail.com>
2026-03-12 15:02:16 +0800

    created image add template and updated urls for it

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import recipe_list, specific_recipe, recipe_add
+from .views import recipe_list, specific_recipe, recipe_add, add_image
 
 urlpatterns = [
     path("recipe/list", recipe_list, name="recipe_list"),
     path("recipe/<int:pk>", specific_recipe, name="specific_recipe"),
-    path('recipe/add', recipe_add, name='recipe_add')
+    path('recipe/add', recipe_add, name='recipe_add'),
+    path('recipe/<int:pk>/add_image', add_image, name='add_image')
 ]
