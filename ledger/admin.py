d4a45a2d8f16ee2e5c0e8aa5a62fa96bac8a3543
Justin <justinheindrich95@gmail.com>
2026-03-12 12:47:18 +0800

    made Inline for RecipeImage under Recipe admin and updated project urls

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Profile, Ingredient, Recipe, RecipeIngredient
+from .models import Profile, Ingredient, Recipe, RecipeIngredient, RecipeImage
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
 
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageInline]
 
 
 admin.site.register(Recipe, RecipeAdmin)
--- recipebook/urls.py
+++ recipebook/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path(
@@ -28,3 +30,5 @@ urlpatterns = [
     path("", include("ledger.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
